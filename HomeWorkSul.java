2940e22ecc474c94182ebb8583d93a2319e4a909
qaAutomations <107888756+qaAutomations@users.noreply.github.com>
2022-10-24 08:45:27 +0300

    Add files via upload

---
+++ HomeWorkSul.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class HomeWorkSul {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //1
+//        int x=5 ;
+//        int y=3;
+//        System.out.println( Math.pow(x,y) );
+
+        //2
+
+//        System.out.println( Math.max(8.99,7) );
+
+        //3
+//        double number = 5.7;
+//        System.out.println( Math.ceil(number) );
+//        System.out.println( Math.floor(number) );
+//        System.out.println( Math.round(number) );
+
+        //4
+//        String name = "itay";
+//        String lastName = "palomo";
+//
+//        System.out.println(name +' '+ lastName);
+
+        //5
+//        System.out.println( Math.sqrt(225) );
+
+
+        //6
+
+//        int a=3,b=4,c=5;
+//        int hekef= a+b+c;
+//        System.out.println( hekef );
+
+        //7
+
+//       double ans2= 4*12* ( -22 / 155.0) ;
+//       double a = 4;
+//       double b = 12;
+//       double c = -22;
+//       double d = 155.0;
+//       double ans3= a*b* (c / d) ;
+//       System.out.println(Math.abs(ans2));
+
+        //8
+
+
+//        System.out.println("enter your name please :");
+//        String name = scanner.next(); // returns the first string to the first space
+//        System.out.println(name.toUpperCase());
+
+        //2.2
+//        System.out.println("please enter your id :");
+//        String id = scanner.next();
+//
+//        System.out.println( id.endsWith("0") );
+
+//        System.out.println("please enter the first word in your mind :");
+//        String word = scanner.next();
+//        System.out.println(word.length());
+
+//        System.out.println("please enter your fav meal :");
+//        String favFood =scanner.nextLine();
+//        System.out.println("how many meals did you had in the last month ?" );
+//        int t = scanner.nextInt();
+//        System.out.println("you ate "+favFood+" "+t+" times this month ");
+
+//        int x1 = scanner.nextInt() ;
+//        int x2 = scanner.nextInt() ;
+//        System.out.println(x1*x2);
+
+//        int x1 = scanner.nextInt() ;
+//        int x2 = scanner.nextInt() ;
+//        System.out.println(x1/x2);
+
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+//        String admin = "Naomi";
+//        String user = scanner.next();
+//
+//        if (admin.equals(user)) {
+//            //True BLock
+//            System.out.println("authorized");
+//        } else {
+//           // false block
+//            System.out.println("not authorized");
+//        }
+//        System.out.println("thank U");
+
+//         int hours = 20 ;
+//
+//         if (hours <= 12){
+//             System.out.println("goood morning");
+//         }
+//         else{
+//             System.out.println("good evening");
+//         }
+
+        // scan a number from the user and print yay if the number greater than 100
+        // and oooooo if the number less or equals to 100
+
+//        int userNumber = scanner.nextInt();
+//
+//        if (userNumber > 100 ){
+//            System.out.println("yay");
+//        }else {
+//            System.out.println("oooooooo");
+//        }
+
+        // write a program that scan a number from the user and checks if
+        // the number divided by 5 equally print yes else print no
+
+//        int number= scanner.nextInt();
+//        if (number % 5 == 0 ){
+//            System.out.println("yes");
+//        }else{
+//            System.out.println("no");
+//        }
+
+        // כתוב תוכנית המקבלת שני מספרים
+        // ומדפס את המספר הגדול ביותר ללא שימוש בפונקציה MATH . MAX
+
+//        System.out.println("please enter a number :");
+//        int x1 = scanner.nextInt();
+//        System.out.println("please enter a number: ");
+//        int x2 = scanner.nextInt();
+//
+//        if (x1 > x2){
+//            System.out.println(x1);
+//        }
+//        else{
+//            System.out.println(x2);
+//        }
+
+        //1- כתבו תוכנית המקבל
+        //מהמשתמש את שמו ואם השם של המשתמש
+        // גדול מ 5 תווים להדפיס "golden name"
+
+//        String username = scanner.next();
+//        if ( username.length() > 5 ){
+//            System.out.println("golden name");
+//        }
+
+
+        //2-כתבו תוכנית המקבלת מהמשתמש את שמו
+        //מהמשתמש את שמו ואם השם של המשתמש גדול מ 5 תווים להדפיס "golden name"
+
+//        String username2 = scanner.next();
+//        if ( username2.endsWith("ma")){
+//            System.out.println("found it");
+//        }else{
+//            System.out.println("try again");
+//        }
+
+        int age = scanner.nextInt();
+
+        if(age <= 50)
+            System.out.println("hi man");
+        else
+            System.out.println("hi sir");
+
+
+
+
+    }
+}
